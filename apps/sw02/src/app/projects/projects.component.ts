d86b6c149a1e86dba2f56886b035bb173b092eda
Steve Witman <stevewitman@gmail.com>
2020-02-25 09:10:56 -0700

    Added methods to projects component

--- apps/sw02/src/app/projects/projects.component.ts
+++ apps/sw02/src/app/projects/projects.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ProjectsService } from '@bb/core-data';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { ProjectsService, Project } from '@bb/core-data';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'bb-projects',
@@ -7,10 +10,81 @@ import { ProjectsService } from '@bb/core-data';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
+  project: Project;
+  projects$: Observable<Project[]>
+  form: FormGroup;
 
-  constructor(private projectsService: ProjectsService) { }
+  constructor(
+    private projectsService: ProjectsService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.getProjects();
+    this.initForm();
+  }
+
+  select(project: Project) {
+    this.project = project;
+    this.form.patchValue(project);
+  }
+
+  save(project: Project) {
+    if (project.id) {
+      this.update(project)
+    } else {
+      this.create(project);
+    }
+  }
+
+  create(project: Project) {
+    this.projectsService.create(project)
+      .subscribe(() => {
+        this.reset();
+        this.getProjects();
+      })
+  }
+
+  update(project: Project) {
+    this.projectsService.update(project)
+      .subscribe(() => {
+        this.reset();
+        this.getProjects();
+      })
+  }
+
+  delete(project: Project) {
+    this.projectsService.delete(project)
+      .subscribe(() => {
+        this.reset();
+        this.getProjects();
+      })
+  }
+
+  private getProjects() {
+    this.projects$ = this.projectsService.all();
+  }
+
+  private initForm() {
+    this.form = this.formBuilder.group({
+      id: null,
+      title: ['', Validators.compose([
+        Validators.required
+      ])],
+      details: ['', Validators.compose([
+        Validators.required
+      ])],
+      importanceLevel: ['', Validators.compose([
+      ])],
+    })
+  }
+
+  private reset() {
+    this.form.reset();
+    this.project = {} as Project;
+    Object.keys(this.form.controls).forEach((key) => {
+      this.form.get(key).setErrors(null);
+    });
   }
 
 }
--- libs/core-data/src/index.ts
+++ libs/core-data/src/index.ts
@@ -1,1 +1,2 @@
 export { ProjectsService } from './lib/projects/projects.service';
+export { Project } from './lib/projects/project.model';
