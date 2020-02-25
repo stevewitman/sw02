660eb3cbc8eb40112e6505152650b5077e015a9e
Steve Witman <stevewitman@gmail.com>
2020-02-25 07:48:02 -0700

    Added material and bootstrap 4 grid utilities

--- apps/sw02/src/app/app.module.ts
+++ apps/sw02/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ProjectsListComponent } from './projects/projects-list/projects-list.component';
 import { ProjectDetailsComponent } from './projects/project-details/project-details.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, ProjectsComponent, ProjectsListComponent, ProjectDetailsComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
