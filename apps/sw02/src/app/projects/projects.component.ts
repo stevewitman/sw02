757452e27d13ce311aacdb7d49bdacf8f33f1b1a
Steve Witman <stevewitman@gmail.com>
2020-02-25 08:40:18 -0700

    Added crud methods to projects service

--- apps/sw02/src/app/projects/projects.component.ts
+++ apps/sw02/src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from '@bb/core-data';
 
 @Component({
   selector: 'bb-projects',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private projectsService: ProjectsService) { }
 
   ngOnInit(): void {
   }
--- libs/core-data/src/lib/projects/projects.service.ts
+++ libs/core-data/src/lib/projects/projects.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Project } from './project.model';
+
+const BASE_URL = 'https://server-30-x-30.herokuapp.com';
+const model = 'projects';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectsService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getUrl() {
+    return `{BASE_URL}/${model}`;
+  }
+
+  getUrlWithId(id: string) {
+    return `${this.getUrl()}/${id}`;
+  }
+
+  all(): Observable<Project[]> {
+    return this.httpClient.get<Project[]>(this.getUrl());
+  }
+
+  findOne(id: string): Observable<Project> {
+    return this.httpClient.get<Project>(this.getUrlWithId(id));
+  }
+
+  create(project: Project): Observable<Project> {
+    return this.httpClient.post<Project>(this.getUrl(), project);
+  }
+
+  update(project: Project): Observable<Project> {
+    return this.httpClient.patch<Project>(this.getUrlWithId(project.id), project);
+  }
+
+  delete(project: Project): Observable<{}> {
+    return this.httpClient.delete<{}>(this.getUrlWithId(project.id));
+  }
 }
