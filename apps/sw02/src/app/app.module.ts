792affd56a73d3dca603a3558fa47259aae4eb58
Steve Witman <stevewitman@gmail.com>
2020-02-25 08:10:35 -0700

    Added UI to toolbar and added shild components to projects component

--- apps/sw02/src/app/app.module.ts
+++ apps/sw02/src/app/app.module.ts
@@ -6,10 +6,21 @@ import { ProjectsComponent } from './projects/projects.component';
 import { ProjectsListComponent } from './projects/projects-list/projects-list.component';
 import { ProjectDetailsComponent } from './projects/project-details/project-details.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UiToolbarModule } from '@bb/ui-toolbar';
+
 
 @NgModule({
-  declarations: [AppComponent, ProjectsComponent, ProjectsListComponent, ProjectDetailsComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    ProjectsComponent,
+    ProjectsListComponent,
+    ProjectDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    UiToolbarModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
